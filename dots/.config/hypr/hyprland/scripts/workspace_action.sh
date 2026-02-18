7644b9dfc2fa56fd6614f37cf74d2e67ca79aed6
James <james.ralph8555@gmail.com>
2026-02-18 17:59:36 -0500

    updated workspace

--- dots/.config/hypr/hyprland/scripts/workspace_action.sh
+++ dots/.config/hypr/hyprland/scripts/workspace_action.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curr_workspace="$(hyprctl activeworkspace -j | jq -r ".id")"
+
 dispatcher="$1"
 shift ## The target is now in $1, not $2
 
@@ -7,11 +7,17 @@ if [[ -z "${dispatcher}" || "${dispatcher}" == "--help" || "${dispatcher}" == "-
   echo "Usage: $0 <dispatcher> <target>"
   exit 1
 fi
+
 if [[ "$1" == *"+"* || "$1" == *"-"* ]]; then ## Is this something like r+1 or -1?
   hyprctl dispatch "${dispatcher}" "$1" ## $1 = workspace id since we shifted earlier.
 elif [[ "$1" =~ ^[0-9]+$ ]]; then ## Is this just a number?
-  hyprctl dispatch "${dispatcher}" "$1" ## Direct workspace switching for single monitor setup
+  curr_workspace="$(hyprctl activeworkspace -j | jq -r ".id")"
+  if [[ "${curr_workspace}" =~ ^[0-9]+$ ]]; then
+    target_workspace=$((((curr_workspace - 1) / 10) * 10 + $1))
+  else
+    target_workspace="$1"
+  fi
+  hyprctl dispatch "${dispatcher}" "${target_workspace}"
 else
   hyprctl dispatch "${dispatcher}" "$1" ## In case the target in a string, required for special workspaces.
-  exit 1
 fi
